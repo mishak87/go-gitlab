76adf0c7a990dc85288a144b64216bc4785e09ef
Kraig Amador <Kraig.Amador@ticketmaster.com>
2014-07-31 15:41:28 -0700

    Added a few more endpoints

--- gitlab/projects_contents.go
+++ gitlab/projects_contents.go
@@ -11,9 +11,40 @@ import (
 	"net/url"
 )
 
+type ProjectFile struct {
+	ID   *string `json:"id,omitempty"`
+	Mode *string `json:"mode,omitempty"`
+	Name *string `json:"name,omitempty"`
+	Type *string `json:"type,omitempty"`
+}
+
+func (p ProjectFile) String() string {
+	return Stringify(p)
+}
+
 // Get a list of repository files and directories in a project.
 //
 // TODO: GitLab API docs: http://doc.gitlab.com/ce/api/repositories.html#list-repository-tree
+func (s *ProjectsService) Tree(projectID int, path string, ref_name string) (*[]ProjectFile, *Response, error) {
+	data := new([]ProjectFile)
+
+	params := url.Values{}
+	params.Add("path", path)
+	params.Add("ref_name", ref_name)
+	u := fmt.Sprintf("projects/%v/repository/tree?%v", projectID, params.Encode())
+
+	req, err := s.client.NewRequest("GET", u, nil)
+	if err != nil {
+		return data, nil, err
+	}
+
+	resp, err := s.client.Do(req, data)
+	if err != nil {
+		return data, resp, err
+	}
+
+	return data, resp, err
+}
 
 // Get the raw file contents for a blob by blob sha.
 //
--- gitlab/projects_files.go
+++ gitlab/projects_files.go
@@ -5,6 +5,20 @@
 
 package gitlab
 
+import (
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+)
+
+type ProjectFileParameters struct {
+	FilePath      string `json:"file_path,omitempty"`
+	BranchName    string `json:"branch_name,omit_empty"`
+	Encoding      string `json:"encoding,omit_empty"`
+	Content       string `json:"content,omit_empty"`
+	CommitMessage string `json:"commit_message,omit_empty"`
+}
+
 // Get file from project
 //
 // TODO: GitLab API docs: http://doc.gitlab.com/ce/api/repository_files.html#get-file-from-repository
@@ -16,6 +30,27 @@ package gitlab
 // Update an existing file in project
 //
 // TODO: GitLab API docs: http://doc.gitlab.com/ce/api/repository_files.html#update-existing-file-in-repository
+func (s *ProjectsService) UpdateFile(projectID int, parameters ProjectFileParameters, content []byte) (interface{}, *Response, error) {
+	u := fmt.Sprintf("projects/%d/repository/files", projectID)
+
+	parameters.Encoding = "base64"
+	parameters.Content = base64.StdEncoding.EncodeToString(content)
+	content, err := json.Marshal(parameters)
+
+	req, err := s.client.NewRequest("PUT", u, parameters)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+	data := new(interface{})
+	resp, err := s.client.Do(req, data)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return data, resp, err
+}
 
 // Delete existing file in project
 //
